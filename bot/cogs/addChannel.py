6fe1fc0b96f3e5266169fda4a932d6aadb840f3c
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-09-13 14:28:22 -0400

    implement archive channels

--- bot/cogs/addChannel.py
+++ bot/cogs/addChannel.py
@@ -1,13 +1,14 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from discord.app_commands import Choice
 import sqlite3
 
 db_path = 'channels.db'
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
+
+#sql methods
 def insertChannel(name:str, id:int, current:int) -> None:
     with conn:
         c.execute(f"INSERT INTO channels VALUES (?, ?, ?)", (name, id, current))
@@ -16,7 +17,15 @@ def getUserCount(name, id):
     c.execute(f"SELECT * FROM channels WHERE name = ? AND id = ?", (name, id))
     return c.fetchone()[2]
 
+def getChannel(name:str):
+    c.execute(
+        f"SELECT * FROM channels WHERE name = ?", (name,))
+    return c.fetchone()
 
+
+"""
+   Adds a channel to the database + creates the channel
+"""
 class addChannel(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
@@ -25,11 +34,14 @@ class addChannel(commands.Cog):
     
     @app_commands.command(name= 'createchannel', description = 'Creates a new channel')
     async def createChannel(self, interaction: discord.Interaction, name:str) -> None:
-        guild = interaction.guild
-        channel = await guild.create_text_channel(f'{name}')
-        insertChannel(name, channel.id, 0)
-        await channel.set_permissions(guild.default_role, view_channel=False)
-        await interaction.response.send_message(f'Channel {channel} created', ephemeral=True)
+        if (getChannel(name) == None):
+            guild = interaction.guild
+            channel = await guild.create_text_channel(f'{name}')
+            insertChannel(name, channel.id, 0)
+            await channel.set_permissions(guild.default_role, view_channel=False)
+            await interaction.response.send_message(f'Channel {channel} created', ephemeral=True)
+        else:
+            await interaction.response.send_message(f'Channel {name} already exists', ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
--- bot/cogs/archive.py
+++ bot/cogs/archive.py
@@ -1,17 +1,40 @@
+import sqlite3
 import discord
 from discord.ext import commands
 from discord import app_commands
+from DiscordChatExporterPy.chat_exporter import chat_exporter
+
+db_path = 'channels.db'
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+
+def removeChannel(id:int):
+    with conn:
+        c.execute(f"DELETE from channels WHERE id = ?", (id,))
 
 
+
+"""
+This is the archive cog. It is responsible for archiving channels.
+"""
 class Archive(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name= 'archive', description = 'archives a channel')
-    async def archive(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message("test")
-
+    async def archive(self, interaction: discord.Interaction, channel:discord.TextChannel) -> None:
+        if (channel.id == interaction.channel.id):
+            await interaction.response.send_message("You might want to archive this from another channel!", ephemeral=True)
+            return
+        if (channel.permissions_for(interaction.user).manage_channels == True ):
+            file = await chat_exporter.quick_export(channel)
+            await interaction.response.send_message(f'Channel {channel} archived', file=file, ephemeral=True)
+            await channel.delete()
+            removeChannel(channel.id)
+        else:
+            interaction.response.send_message("You do not have permission to archive this channel", ephemeral=True)
+            return
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Archive(bot))
